e1d4627fdbbedc5b837cf01b3356bb059cd7fd62
tamagoru <marushannnn@outlook.com>
2023-05-23 19:03:25 -0300

    Add project files.

---
+++ Sincronico-1/Form1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sincronico_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
